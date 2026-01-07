ea7ef66726d6549c2c7a5aa04060779498648231
marccnfs <conseiller.numeriquesjb@gmail.com>
2026-01-07 11:43:20 +0100

    micro maj form et step

--- src/Form/EscapeGameType.php
+++ src/Form/EscapeGameType.php
@@ -8,6 +8,7 @@ use App\Entity\EscapeGame;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
